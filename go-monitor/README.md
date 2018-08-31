7c296b212b1bd94ed870293dcd1cd16c801c4d45
RYO Tsukayama <e155728@ie.u-ryukyu.ac.jp>
2018-08-31 17:38:07 +0900

    first commit & add monitor tool

---
+++ go-monitor/README.md
@@ -0,0 +1,16 @@
+# server-tool
+tool for server hundle
+
+## USEAG
+
+$ screen
+$ go run monitor_string.go MONITOR_TARGET_FILE
+
+or
+
+$ go run monitor_string.go MONITOR_TARGET_FILE &
+
+## OUTPUT_LOG
+
+./log/res_UNCHANGETIME.txt
+
---
+++ go-monitor/monitor_num.go
@@ -0,0 +1,75 @@
+package main
+
+import(
+		"fmt"
+		"os/exec"
+		"strings"
+		"flag"
+		"strconv"
+		"time"
+		"os"
+)
+
+var Timestamp int = 0
+var counter int = 0
+
+
+func main(){
+		flag.Parse()
+		args := flag.Args()
+		FILENAME := args[0]
+
+		//fmt.Println(FILENAME)
+		//fmt.Println("get() : ",get(FILENAME))
+		Timestamp = get(FILENAME)
+
+		//fmt.Println(reflect.TypeOf(Timestamp))
+
+		file, _ := os.Create("res_"+strconv.Itoa(Timestamp)+".txt")
+		defer file.Close()
+
+		FirstTime := fmt.Sprint("Starting-Timestamp : ",Timestamp)
+		FirstTime = FirstTime + "\n"
+		//fmt.Println(FirstTime)
+
+		fmt.Fprint(file,FirstTime)
+
+		time.Sleep(1 * time.Second)
+
+		for{
+				if Timestamp != get(FILENAME){
+						//fmt.Println("OverWriting file now !! : ",get(FILENAME))
+						WrittenTime := fmt.Sprint("OverWriting file now !! : ",get(FILENAME))
+						WrittenTime = WrittenTime + "\n"
+						//fmt.Println(WrittenTime)
+						if counter == 1{
+								fmt.Fprint(file,WrittenTime)
+						}else{
+								if counter == 5{
+								os.Exit(0)
+								}
+						}
+						counter += 1
+				}
+
+		time.Sleep(1 * time.Second)
+		}
+}
+
+func get(FILENAME string)int{
+		res , err := exec.Command("ls","-la",FILENAME).Output()
+		if err != nil{
+				fmt.Println("error : ",err)
+		}
+		convert := string(res)
+		//fmt.Println(convert)
+
+		array := strings.Split(convert," ")
+		//fmt.Println("array : ",array)
+		splited := strings.Split(array[11],":")
+		//fmt.Println(array[9]+array[10]+array[6]+splited[0]+splited[1])
+		resint, _  := strconv.Atoi(array[9]+array[10]+array[6]+splited[0]+splited[1])
+		return resint
+}
+
+
